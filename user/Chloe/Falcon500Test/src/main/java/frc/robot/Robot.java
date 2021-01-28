1f0e1ab2119b58a7c69738207b0155249d0117b5
David Pieper <david.pieper123@gmail.com>
2021-01-28 17:50:29 -0600

    Tested code for all 4 TalonFx controllers New code for all 4 TalonFx controllers has been tested using the differential drive class and confirmed to be working.

--- user/Chloe/Falcon500Test/src/main/java/frc/robot/Robot.java
+++ user/Chloe/Falcon500Test/src/main/java/frc/robot/Robot.java
@@ -5,9 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically it contains the code
@@ -16,17 +19,18 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
   private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  private SpeedController m_leftMotorController, m_rightMotorController;
 
   @Override
   public void robotInit() {
-    m_myRobot = new DifferentialDrive(new WPI_TalonFX(10), new WPI_TalonFX(11));
+    m_leftMotorController = new SpeedControllerGroup(new WPI_TalonFX(10), new WPI_TalonFX(12));
+    m_rightMotorController = new SpeedControllerGroup(new WPI_TalonFX(11), new WPI_TalonFX(13));
+    m_myRobot = new DifferentialDrive(m_leftMotorController, m_rightMotorController);
     m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    m_myRobot.tankDrive(m_leftStick.getRawAxis(1), m_leftStick.getRawAxis(5));
   }
 }

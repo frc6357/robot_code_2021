cc593a3734a28a5cffc36196022157cb8da94c4f
vivek-ramadhar <vivek.ramadhar@gmail.com>
2021-01-30 14:27:35 -0600

    spline constraints defined, starting to stitch together splines

--- user/vivek/PythonCubicSplineTrajectory/PythonCubicSplineGeneration/src/loadJson.py
+++ user/vivek/PythonCubicSplineTrajectory/PythonCubicSplineGeneration/src/loadJson.py
@@ -1,34 +1,39 @@
 import json
 import numpy as np
-import math
-
-
-# Takes in a file in the same directory as the python script and loads the JSON into the json_obj variable
+from cubicspline_test_tbd import cubicSplineSegment
+# Takes in a JSON file in the same directory as the python script and loads the JSON into the json_obj variable as a
+# list of dictionaries of the format {'x' : (int), 'y' : (int), 'theta' : (float) }
 with open("inputFile.json", "r") as file:
     json_obj = json.load(file)
+points = [dictObj for dictObj in json_obj.items()][0][1]
 
-# Takes in JSON file and iterates through the array and outputs a chunk of each point and angle (x, y, theta)
-arr = [dictObj for dictObj in json_obj.items()][0]
-arr = [tuple(i.values()) for i in arr[1]]
+# iterates through the list of points and groups into x values and y values of the current and next point in the list
+# also assigns the variable for the angles of the current and next point of the list
+for i in range(len(points)):
+    if i < len(points) - 1:
+        xs = np.array([int(points[i]['x']), int(points[i+1]['x'])])
+        ys = np.array([int(points[i]['y']), int(points[i + 1]['y'])])
+        start_angle = float(points[i]['theta'])
+        end_angle = float(points[i+1]['theta'])
 
-# Takes in the list of points and angles and groups the current tuple and the next tuple together under one tuple
-# ( (x1, y1, theta1), (x2, y2, theta2) )
-# Does this for each coordinate and angle in the JSON file
-points_angle =[]
-for i in range(len(arr)):
-    if i < len(arr) -1:
-        e1 = arr[i]
-        e2 = arr[i+1]
-        x = (e1, e2)
-        points_angle.append(x)
-print(points_angle)
-
-
-for i in points_angle:
+        string = "segment" + str(i + 1)
+        splinePath = {}
+        splineSegment = cubicSplineSegment(xs, ys, start_angle, end_angle)
+        splinePath[string] = splineSegment
 
 
-    # splits the tuple into each individual chunk and assign each x and y-value and angle to a variable for each chunk
-    p1 = i[0]
-    p2 = i[1]
-    x1, y1, a1 = p1
-    x2, y2, a2 = p2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

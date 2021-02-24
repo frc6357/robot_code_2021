90da80b0042fbd15a074e99ef2183a5bcdd30431
Shaun Kurian <shaunkurian345@gmail.com>
2021-02-23 18:00:53 -0600

    Updated documentation of intake direction toggling

--- src/main/java/frc/robot/commands/ToggleIntakeDirectionCommand.java
+++ src/main/java/frc/robot/commands/ToggleIntakeDirectionCommand.java
@@ -8,16 +8,17 @@ import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SK21Intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
+/** An intake command that reverses the direction 
+ * of the intake rollers when it is extended. */
 public class ToggleIntakeDirectionCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final SK21Intake m_subsystem;
-  private boolean goingForwards;
 
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new ToggleIntakeDirectionCommand which takes in the
+   * required subsystem
    *
-   * @param subsystem The subsystem used by this command.
+   * @param subsystem The intake subsystem used by this command.
    */
   public ToggleIntakeDirectionCommand(SK21Intake subsystem) {
     m_subsystem = subsystem;
@@ -26,6 +27,9 @@ public class ToggleIntakeDirectionCommand extends CommandBase {
   }
 
   // Called when the command is initially scheduled.
+  /** It will set the intake to go to the right direction
+   *  and will only set it if the intake is extended out
+   */
   @Override
   public void initialize() {
 
@@ -50,7 +54,8 @@ public class ToggleIntakeDirectionCommand extends CommandBase {
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
+  // Returns true when the command should end, which should always be true
+  // as the functionality ends immediately after the intialize function.
   @Override
   public boolean isFinished() {
     return true;

66176ecb34edd105c6c7b45251425c8c4e56de6e
Justin Garrity <firejustinince2@gmail.com>
2021-02-27 10:12:35 -0600

    Added Retract and Extend intake commands

---
+++ src/main/java/frc/robot/commands/ExtendIntake.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SK21Intake;
+
+/**
+ * This command toggles the intake position to be extended or retracted.
+ */
+public class ExtendIntake extends CommandBase 
+{
+    private final SK21Intake subsystem;
+
+    public ExtendIntake(SK21Intake subsystem) 
+    {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        this.subsystem = subsystem;
+        addRequirements(subsystem);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    public void initialize() 
+    {
+        subsystem.extendIntake();
+        subsystem.startIntakeRoller();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() 
+    {
+        return true;
+    }
+}
--- src/main/java/frc/robot/commands/RetractIntake.java
+++ src/main/java/frc/robot/commands/RetractIntake.java
@@ -13,11 +13,11 @@ import frc.robot.subsystems.SK21Intake;
 /**
  * This command toggles the intake position to be extended or retracted.
  */
-public class ToggleIntakeCommand extends CommandBase 
+public class RetractIntake extends CommandBase 
 {
     private final SK21Intake subsystem;
 
-    public ToggleIntakeCommand(SK21Intake subsystem) 
+    public RetractIntake(SK21Intake subsystem) 
     {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -28,17 +28,9 @@ public class ToggleIntakeCommand extends CommandBase
     // Called just before this Command runs the first time
     @Override
     public void initialize() 
-    {
-        if(subsystem.isIntakeExtended())
-        {
-            subsystem.retractIntake();
-            subsystem.stopIntakeRoller();
-        }
-        else
-        {
-            subsystem.extendIntake();
-            subsystem.startIntakeRoller();
-        }
+    { 
+        subsystem.retractIntake();
+        subsystem.stopIntakeRoller();
     }
 
     // Make this return true when this Command no longer needs to run execute()

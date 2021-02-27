c0091f5b9cedc470671760f8cac4af432c80ee18
thpr <thpr74@gmail.com>
2021-02-27 14:26:51 -0600

    Cleanup

--- src/main/java/frc/robot/commands/TestIntakeCommand.java
+++ src/main/java/frc/robot/commands/TestIntakeCommand.java
@@ -6,18 +6,30 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SK21Intake;
 
+/**
+ * A Command for
+ */
 public class TestIntakeCommand extends CommandBase
 {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  /**
+   * The Intake subsystem under test.
+   */
   private final SK21Intake intakeSubsystem;
 
+  /**
+   * NetworkTableEntry for the Deployment Solenoid.
+   */
   private NetworkTableEntry intakeDeployEntry;
+
+  /**
+   * NetworkTableEntry for the Roller Motor.
+   */
   private NetworkTableEntry intakeRollerEntry;
 
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new TestIntakeCommand for thegiven intakeSubsystem.
    *
-   * @param subsystem
+   * @param intakeSubsystem
    *          The subsystem used by this command.
    */
   public TestIntakeCommand(SK21Intake intakeSubsystem)
@@ -31,35 +43,15 @@ public class TestIntakeCommand extends CommandBase
   @Override
   public void initialize()
   {
-    intakeDeployEntry = Shuffleboard.getTab("Intake")
-            .add("extension",1)
-            .withWidget(BuiltInWidgets.kToggleButton)
-            .withSize(2,1)
-            .withPosition(0, 0)
-            .getEntry();
+    intakeDeployEntry = Shuffleboard.getTab("Intake").add("extension", 1)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(2, 1)
+      .withPosition(0, 0).getEntry();
 
-        intakeRollerEntry = Shuffleboard.getTab("Intake")
-            .add("roller",3)
-            .withWidget(BuiltInWidgets.kNumberSlider)
-            .withSize(1,1)
-            .withPosition(0,4)
-            .getEntry();
-
+    intakeRollerEntry = Shuffleboard.getTab("Intake").add("roller", 3)
+      .withWidget(BuiltInWidgets.kNumberSlider).withSize(1, 1)
+      .withPosition(0, 4).getEntry();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute()
-  {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted)
-  {
-  }
-
-  // Returns true when the command should end.
   @Override
   public boolean isFinished()
   {

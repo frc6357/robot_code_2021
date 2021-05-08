3c09728e4df6244ccd6361a62ecffa23bef26a84
thpr <thpr74@gmail.com>
2021-05-08 14:37:57 -0500

    Docs

--- src/main/java/frc/robot/subsystems/TestModeManager.java
+++ src/main/java/frc/robot/subsystems/TestModeManager.java
@@ -3,38 +3,65 @@ package frc.robot.subsystems;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Manager for Spring Konstant's Test Mode Control System.
+ * 
+ * Manages registration of Subsystems and delegates initialize and periodic methods to all
+ * registered subsystems.
+ */
 public final class TestModeManager
 {
 
-  private static List<SKSubsystemBase> subsystemList = new ArrayList<>();
-  private static boolean testModeIntialized = false;
-
-  public static void registerSubsystem(SKSubsystemBase skSubsystemBase)
-  {
-    subsystemList.add(skSubsystemBase);
-  }
-
-  public static void initializeTestMode()
-  {
-    if (!testModeIntialized)
+    private TestModeManager()
     {
-      for (SKSubsystemBase skSubsystemBase : subsystemList)
-      {
-        skSubsystemBase.initializeTestMode();
-      }
-      testModeIntialized = true;
+        //Do not instantiate Utility Class
     }
-    for (SKSubsystemBase skSubsystemBase : subsystemList)
-    {
-      skSubsystemBase.enterTestMode();
-    }
-  }
 
-  public static void testModePeriodic()
-  {
-    for (SKSubsystemBase skSubsystemBase : subsystemList)
+    private static List<SKSubsystemBase> subsystemList = new ArrayList<>();
+    private static boolean testModeIntialized = false;
+
+    /**
+     * Register an SKSubsystemBase with the TestModeManager.
+     * 
+     * @param skSubsystemBase
+     *            The SKSubsystemBase to be registered
+     */
+    public static void registerSubsystem(SKSubsystemBase skSubsystemBase)
     {
-      skSubsystemBase.testModePeriodic();
+        subsystemList.add(skSubsystemBase);
     }
-  }
+
+    /**
+     * Initialize the registered SKSubsystemBase objects to run in Test Mode.
+     * 
+     * In general, this should be called from Robot.java testInit().
+     */
+    public static void initializeTestMode()
+    {
+        if (!testModeIntialized)
+        {
+            for (SKSubsystemBase skSubsystemBase : subsystemList)
+            {
+                skSubsystemBase.initializeTestMode();
+            }
+            testModeIntialized = true;
+        }
+        for (SKSubsystemBase skSubsystemBase : subsystemList)
+        {
+            skSubsystemBase.enterTestMode();
+        }
+    }
+
+    /**
+     * Call the periodic method on all registered SKSubsystemBase objects.
+     * 
+     * In general, this should be called from Robot.java testPeriodic().
+     */
+    public static void testModePeriodic()
+    {
+        for (SKSubsystemBase skSubsystemBase : subsystemList)
+        {
+            skSubsystemBase.testModePeriodic();
+        }
+    }
 }

609fcb46e972b3cc8eafb165038f4ef9ee8cbc31
thpr <thpr74@gmail.com>
2021-05-08 14:15:42 -0500

    Cleanup Test Mode System

--- src/main/java/frc/robot/subsystems/TestModeManager.java
+++ src/main/java/frc/robot/subsystems/TestModeManager.java
@@ -3,40 +3,37 @@ package frc.robot.subsystems;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class TestModeManager {
+public final class TestModeManager
+{
 
   private static List<SKSubsystemBase> subsystemList = new ArrayList<>();
-  static boolean testModeIntialized = false;
-  
+  private static boolean testModeIntialized = false;
 
   public static void registerSubsystem(SKSubsystemBase skSubsystemBase)
   {
     subsystemList.add(skSubsystemBase);
   }
 
-  public static void initializeTestMode(){
-    if (testModeIntialized == false)
+  public static void initializeTestMode()
+  {
+    if (!testModeIntialized)
     {
-      for (SKSubsystemBase skSubsystemBase : subsystemList) 
+      for (SKSubsystemBase skSubsystemBase : subsystemList)
       {
         skSubsystemBase.initializeTestMode();
-        skSubsystemBase.enterTestMode();
-        testModeIntialized = true;
       }
+      testModeIntialized = true;
     }
-    else
+    for (SKSubsystemBase skSubsystemBase : subsystemList)
     {
-      for (SKSubsystemBase skSubsystemBase : subsystemList) 
-      {
-        skSubsystemBase.initializeTestMode();
-        skSubsystemBase.enterTestMode();
-        testModeIntialized = true;
-      }
+      skSubsystemBase.enterTestMode();
     }
   }
 
-  public static void testModePeriodic(){
-    for (SKSubsystemBase skSubsystemBase : subsystemList) {
+  public static void testModePeriodic()
+  {
+    for (SKSubsystemBase skSubsystemBase : subsystemList)
+    {
       skSubsystemBase.testModePeriodic();
     }
   }

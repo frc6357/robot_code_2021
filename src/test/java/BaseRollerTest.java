c14d9a833abbbe9ad8f44000cba314312663a310
Dave Wilson <dave@webartz.com>
2021-07-31 14:21:20 -0500

    Add simple speed controller model and unit test for BaseRoller class

---
+++ src/test/java/BaseRollerTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import edu.wpi.first.hal.HAL;
+import frc.robot.subsystems.base.BaseRoller;
+import edu.wpi.first.wpilibj.SpeedController;
+import org.junit.*;
+
+public class BaseRollerTest {
+  public static final double SPEED = 0.5;
+  private TestSpeedController controller = new TestSpeedController();
+  private BaseRoller drive;
+
+  @Before // this method will run before each test
+  public void setup() {
+    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
+    drive = new BaseRoller(controller, SPEED); // create our roller object
+  }
+
+  @After // this method will run after each test
+  public void shutdown() throws Exception {
+    
+  }
+
+  @Test
+  public void testStop() {
+    drive.setStop();
+    assertEquals(0.0, drive.getSpeed(), 0.0); // Make sure the motor has been stopped.
+  }
+
+  @Test
+  public void testSetForwards() {
+    drive.setForwards();
+    assertEquals(SPEED, drive.getSpeed(), 0.0); // Make sure the motor is running forwards.
+  }
+
+  @Test
+  public void testSetBackwards() {
+    drive.setBackwards();
+    assertEquals(-SPEED, drive.getSpeed(), 0.0); // Make sure the motor is running backwards.
+  }
+}
---
+++ src/test/java/TestSpeedController.java
@@ -0,0 +1,85 @@
+// 
+// A stub class implementing the SpeedController interface which provides us with a very
+// simple motor controller model for use in unit testing.
+//
+import edu.wpi.first.wpilibj.SpeedController;
+
+/** Interface for speed controlling devices. */
+public class TestSpeedController implements SpeedController {
+  private double currentSpeed          = 0.0;
+  private static double batteryVoltage = 12.0;
+  private boolean inverted              = false;
+
+  /**
+   * Common interface for setting the speed of a speed controller.
+   *
+   * @param speed The speed to set. Value should be between -1.0 and 1.0.
+   */
+  public void set(double speed)
+  {
+    currentSpeed = speed;
+  }
+
+  /**
+   * Sets the voltage output of the SpeedController. Compensates for the current bus voltage to
+   * ensure that the desired voltage is output even if the battery voltage is below 12V - highly
+   * useful when the voltage outputs are "meaningful" (e.g. they come from a feedforward
+   * calculation).
+   *
+   * <p>NOTE: This function *must* be called regularly in order for voltage compensation to work
+   * properly - unlike the ordinary set function, it is not "set it and forget it."
+   *
+   * @param outputVolts The voltage to output.
+   */
+  public void setVoltage(double outputVolts) {
+    set(outputVolts / batteryVoltage);
+  }
+
+  /**
+   * Common interface for getting the current set speed of a speed controller.
+   *
+   * @return The current set speed. Value is between -1.0 and 1.0.
+   */
+  public double get() {
+    return currentSpeed;
+  }
+
+  /**
+   * Common interface for inverting direction of a speed controller.
+   *
+   * @param isInverted The state of inversion true is inverted.
+   */
+  public void setInverted(boolean isInverted) {
+    inverted = isInverted;
+  }
+
+  /**
+   * Common interface for returning if a speed controller is in the inverted state or not.
+   *
+   * @return isInverted The state of the inversion true is inverted.
+   */
+  public boolean getInverted()
+  {
+    return inverted;
+  }
+
+  /** Disable the speed controller. */
+  public void disable()
+  {
+    currentSpeed = 0.0;
+  }
+
+  /**
+   * Stops motor movement. Motor can be moved again by calling set without having to re-enable the
+   * motor.
+   */
+  public void stopMotor()
+  {
+    currentSpeed = 0.0;
+  }
+
+  public void pidWrite(double output)
+  {
+    
+  }
+}
